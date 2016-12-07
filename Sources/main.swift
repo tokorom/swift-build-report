0e7933e2d4f16f3d4ec10e546e350b58d2a9188a
tokorom <tokorom@gmail.com>
2016-12-07 17:52:14 +0900

    Add some colors

--- Sources/main.swift
+++ Sources/main.swift
@@ -2,8 +2,14 @@ import Foundation
 
 public enum AnsiColor {
     case reset
+    case black
     case red
     case green
+    case yellow
+    case blue
+    case magenda
+    case cyan
+    case white
 
     private static let prefix = "\u{001B}[0;"
     private static let suffix = "m"
@@ -15,8 +21,14 @@ public enum AnsiColor {
     private var index: String {
         switch self {
         case .reset: return "0"
+        case .black: return "30"
         case .red: return "31"
         case .green: return "32"
+        case .yellow: return "33"
+        case .blue: return "34"
+        case .magenda: return "35"
+        case .cyan: return "36"
+        case .white: return "37"
         }
     }
 }
@@ -61,8 +73,12 @@ class Format {
 
 class Formatter {
     lazy var formats: [Format] = [
+        // swift build
         Format(kind: .linking, pattern: "^Linking ", replacer: .color(.green)),
         Format(kind: .compileSwiftModule, pattern: "^Compile Swift Module ", replacer: .color(.green)),
+        // npm
+        Format(kind: .npmError, pattern: "^npm ERR! ", replacer: .color(.red)),
+        Format(kind: .npmWarning, pattern: "^npm WARN ", replacer: .color(.yellow)),
     ]
 
     func parse(line: String) -> FormattedLine {
@@ -86,6 +102,9 @@ struct FormattedLine {
         case linking
         case compileSwiftModule
         case buildErrorSummary
+
+        case npmError
+        case npmWarning
     }
 }
 
