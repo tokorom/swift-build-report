f71a7459d0701855f5cb3319c288c33f029af2eb
tokorom <tokorom@gmail.com>
2016-12-09 14:20:47 +0900

    Support some formatters

--- Sources/SwiftBuildReport/DefaultFormatter.swift
+++ Sources/SwiftBuildReport/DefaultFormatter.swift
@@ -8,9 +8,21 @@ import Foundation
 
 class DefaultFormatter: Formatter {
     lazy var formats: [Format] = [
-        Format(kind: .linking, pattern: "^Linking ", replacer: .color(.green)),
+        Format(kind: .testSuiteStarted, pattern: "^Test Suite .* started ", replacer: .color(.cyan)),
+        Format(kind: .testCaseStarted, pattern: "^Test Case .* started.$", replacer: .color(.cyan)),
+        Format(kind: .testErrorPoint, pattern: " *\\^~* *", replacer: .color(.cyan)),
+        Format(kind: .testError, pattern: "^\\/.* failed: .*- *$", replacer: .patterns([
+            ReplacePattern(pattern: " \\([^()]+\\) ", ansiColor: .blue),
+            ReplacePattern(pattern: "^.*$", ansiColor: .red),
+        ])),
+        Format(kind: .testSummary, pattern: " Executed [0-9]+ tests?, with [0-9]+ failures? ", replacer: .patterns([
+            ReplacePattern(pattern: "0 failures ", ansiColor: .green),
+            ReplacePattern(pattern: "[1-9][0-9]* failures? ", ansiColor: .red),
+        ])),
+        Format(kind: .compileError, pattern: "^\\/.* error: ", replacer: .color(.red)),
         Format(kind: .compileStart, pattern: "^Compile Swift Module ", replacer: .color(.cyan)),
-        Format(kind: .compileError, pattern: "^\\/", replacer: .color(.red)),
+        Format(kind: .linking, pattern: "^Linking ", replacer: .color(.green)),
+        Format(kind: .buildSummaryWithError, pattern: " error: build had [0-9]+ command failures?$", replacer: .color(.red)),
     ]
 
     func format(line: String) -> String {
@@ -25,6 +37,25 @@ class DefaultFormatter: Formatter {
             }
         }
         return FormattedLine(kind: .normal, line: line)
+    }
+}
+
+struct FormattedLine {
+    let kind: Kind
+    let line: String
+
+    enum Kind {
+        case testSuiteStarted
+        case testCaseStarted
+        case testError
+        case testErrorPoint
+        case testSummary
+        case compileError
+        case compileErrorSupplement
+        case linking
+        case compileStart
+        case buildSummaryWithError
+        case normal
     }
 }
 
@@ -50,29 +81,44 @@ class Format {
     }
 }
 
-extension Format {
-    enum Replacer {
-        case color(AnsiColor)
+class ReplacePattern {
+    let pattern: String
+    let ansiColor: AnsiColor
 
-        func replace(line: String) -> String {
-            switch self {
-            case .color(let color):
-                return color.text + line + AnsiColor.reset.text
-            }
+    lazy var regexp: NSRegularExpression? = try? NSRegularExpression(pattern: self.pattern)
+
+    init(pattern: String, ansiColor: AnsiColor) {
+        self.pattern = pattern
+        self.ansiColor = ansiColor
+    }
+
+    func replace(line: String) -> String {
+        let range = NSRange(location: 0, length: line.characters.count)
+        let template = ansiColor.text + "$0" + AnsiColor.reset.text
+        guard let result = regexp?.stringByReplacingMatches(in: line, range: range, withTemplate: template) else {
+            return line
         }
+        return result
     }
 }
 
-struct FormattedLine {
-    let kind: Kind
-    let line: String
+enum Replacer {
+    case color(AnsiColor)
+    case patterns([ReplacePattern])
+    case custom((String) -> String)
 
-    enum Kind {
-        case normal
-        case compileError
-        case compileErrorSupplement
-        case linking
-        case compileStart
-        case buildErrorSummary
+    func replace(line: String) -> String {
+        switch self {
+        case .color(let color):
+            return color.text + line + AnsiColor.reset.text
+        case .patterns(let replacePatterns):
+            var result = line
+            for replacePattern in replacePatterns {
+                result = replacePattern.replace(line: result)
+            }
+            return result
+        case .custom(let replaceFunction):
+            return replaceFunction(line)
+        }
     }
 }
---
+++ Tests/SwiftBuildReportTests/DefaultFormatterTests.swift
@@ -0,0 +1,118 @@
+//
+//  DefaultFormatterTests.swift
+//
+//  Created by ToKoRo on 2016-12-09.
+//
+
+import XCTest
+@testable import SwiftBuildReport
+
+class DefaultFormatterTests: XCTestCase {
+    let subject = DefaultFormatter()
+
+    func testNormalLine() {
+        let line = "foo bar"
+        let expected: FormattedLine.Kind = .normal
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testCompileStart() {
+        let line = "Compile Swift Module 'SwiftBuildReport' (7 sources)"
+        let expected: FormattedLine.Kind = .compileStart
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testCompileError() {
+        let line = "/Users/tokorom/develop/github/swift-build-report/Sources/SwiftBuildReport/main.swift:31:1: error: foo bar"
+        let expected: FormattedLine.Kind = .compileError
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testLinking() {
+        let line = "Linking ./.build/debug/SwiftBuildReport"
+        let expected: FormattedLine.Kind = .linking
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testBuildErrorSummary() {
+        let line = "<unknown>:0: error: build had 1 command failures"
+        let expected: FormattedLine.Kind = .buildSummaryWithError
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testTestSuiteStarted() {
+        let line = "Test Suite 'DefaultFormatterTests' started at 2016-12-09 11:09:14.488"
+        let expected: FormattedLine.Kind = .testSuiteStarted
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testTestCaseStarted() {
+        let line = "Test Case '-[SwiftBuildReportTests.AnsiColorTests testGreen]' started."
+        let expected: FormattedLine.Kind = .testCaseStarted
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testTestError() {
+        let line = "/Users/tokorom/develop/github/swift-build-report/Tests/SwiftBuildReportTests/DefaultFormatterTests.swift"
+            + ":45: error: -[SwiftBuildReportTests.DefaultFormatterTests testBuildErrorSummary] : "
+            + "XCTAssertEqual failed: (\"normal\") is not equal to (\"buildSummaryWithError\") -"
+        let expected: FormattedLine.Kind = .testError
+
+        let formatted = subject.parse(line: line)
+        let kind = formatted.kind
+        let result = formatted.line
+
+        XCTAssertEqual(kind, expected)
+        XCTAssertEqual(
+            result,
+            "\u{001B}[0;31m/Users/tokorom/develop/github/swift-build-report/Tests/SwiftBuildReportTests/"
+                + "DefaultFormatterTests.swift"
+                + ":45: error: -[SwiftBuildReportTests.DefaultFormatterTests testBuildErrorSummary] : "
+                + "XCTAssertEqual failed:\u{001B}[0;34m (\"normal\") \u{001B}[0;0mis not equal to"
+                + "\u{001B}[0;34m (\"buildSummaryWithError\") \u{001B}[0;0m-\u{001B}[0;0m"
+        )
+    }
+
+    func testErrorPoint() {
+        let line = "  ^  "
+        let expected: FormattedLine.Kind = .testErrorPoint
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testErrorPointLong() {
+        let line = "  ^~~~~~  "
+        let expected: FormattedLine.Kind = .testErrorPoint
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testTestErrorWithLongMessage() {
+        let line = "/Users/tokorom/develop/github/swift-build-report/Tests/SwiftBuildReportTests/DefaultFormatterTests.swift:80"
+            + ": error: -[SwiftBuildReportTests.DefaultFormatterTests testTestSummary] : XCTAssertEqual failed: (\"    Exec"
+            + "uted 10 tests, with 2 failuers (0 unexpected) in 0.008 (0.009) seconds\") is not equal to (\"    Executed 10 tests"
+            + ", with 2 failuers (0 unexpected) in 0.008 (0.009) seconds\") -"
+        let expected: FormattedLine.Kind = .testError
+        let kind = subject.parse(line: line).kind
+        XCTAssertEqual(kind, expected)
+    }
+
+    func testTestSummary() {
+        let line = "    Executed 10 tests, with 1 failure (0 unexpected) in 0.008 (0.009) seconds"
+        let expected: FormattedLine.Kind = .testSummary
+
+        let formatted = subject.parse(line: line)
+        let kind = formatted.kind
+        let result = formatted.line
+
+        XCTAssertEqual(kind, expected)
+        XCTAssertEqual(result, "    Executed 10 tests, with \u{001B}[0;31m1 failure \u{001B}[0;0m(0 unexpected) in 0.008 (0.009) seconds")
+    }
+}
