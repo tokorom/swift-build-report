4fa44700e0497dd454162dbad15d28cb100da74b
tokorom <tokorom@gmail.com>
2016-12-12 13:43:08 +0900

    Add images

--- Package.swift
+++
@@ -1,8 +0,0 @@
-import PackageDescription
-
-let package = Package(
-    name: "swift-build-report",
-    dependencies: [
-        .Package(url: "https://github.com/kylef/Commander.git", majorVersion: 0),
-    ]
-)
